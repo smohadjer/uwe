eedebc9e88820165b6c2fdd906d1fd0b08c12cf7
Saeid Mohadjer <saeid@fastmail.fm>
2022-11-13 21:36:20 +0100

    fixes js error

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -1,52 +1,39 @@
-(function() {
+import Navigation from './modules/nav.js';
+
+//Helper classes to HTML for styling of nojs version
+const html = document.querySelector('html');
+html.classList.remove('no-js');
+html.classList.add('js');
+
+//taken from http://youmightnotneedjquery.com/
+function ready(fn) {
 	'use strict';
 
-	//Helper classes to HTML for styling of nojs version
-	const html = document.querySelector('html');
-	html.classList.remove('no-js');
-	html.classList.add('js');
-
-	//taken from http://youmightnotneedjquery.com/
-	function ready(fn) {
-		if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
-			fn();
-		} else {
-			document.addEventListener('DOMContentLoaded', fn);
-		}
+	if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
 	}
+}
 
-	ready(function() {
-		console.log('DOM is ready!');
+ready(function() {
+	'use strict';
 
-		//initialize navigation
-		const nav = new Navigation({
-			element: document.querySelector('header > nav')
-		});
+	console.log('DOM is ready!');
 
-		//hamburger button
-		const hamburger = document.querySelector('button.hamburger');
-		if (hamburger) {
-			hamburger.addEventListener('click', function() {
-				hamburger.classList.toggle('is-active');
-
-				if (nav.el) {
-					nav.el.classList.toggle('is-visible');
-				}
-			});
-		}
-
-		//example of using a handlebars template/partial
-		/*
-		Handlebars.registerPartial('myPartial', myApp.templates.myPartial);
-		var template = myApp.templates.helloWorld;
-		var html = template({
-			'title': 'Example of markup generated via js using handlebars',
-			'subtitle': 'This text comes from a hbs partial!'
-		});
-		var aside = document.querySelector('aside');
-		if (aside) {
-			aside.innerHTML = html;
-		}
-		*/
+	//initialize navigation
+	const nav = new Navigation({
+		element: document.querySelector('header > nav')
 	});
-})();
+
+	//hamburger button
+	const hamburger = document.querySelector('button.hamburger');
+	if (hamburger) {
+		hamburger.addEventListener('click', function() {
+			hamburger.classList.toggle('is-active');
+			if (nav.el) {
+				nav.el.classList.toggle('is-visible');
+			}
+		});
+	}
+});
